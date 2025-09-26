b0fbdcf3e611a53077ddbf14f6247ee886049bff
James Heaton <heaton1138@gmail.com>
2025-09-26 13:45:16 -0500

    Styling bug fixed edit user

--- src/components/users/EditUserProfile.jsx
+++ src/components/users/EditUserProfile.jsx
@@ -2,7 +2,7 @@ import { EditUserForm } from "../forms/UserForm";
 
 export const EditUserProfile = () => {
   return (
-    <div className="edit-user">
+    <div id="edit-user-mobile" className="edit-user">
       
       <div className="edit-user-form">
         <h1 className="edit-header">Edit Profile</h1>
